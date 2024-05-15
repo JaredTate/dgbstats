d846db0970a82b5cefc6e52bf59354e1eb8e3125
Jared Tate <13957390+JaredTate@users.noreply.github.com>
2024-05-15 16:25:06 -0600

    Ports

--- src/config.js
+++ src/config.js
@@ -4,11 +4,11 @@ const config = {
       wsBaseUrl: 'ws://localhost:5002',
     },
     production: {
-      apiBaseUrl: 'http://digibyte.io',
-      wsBaseUrl: 'ws://digibyte.io',
+      apiBaseUrl: 'http://digibyte.io:5001',
+      wsBaseUrl: 'ws://digibyte.io:5002',
     },
   };
   
-  const env = process.env.NODE_ENV || 'development';
+  const env = process.env.NODE_ENV || 'production';
   
   export default config[env];
