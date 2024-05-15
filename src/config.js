9d585c658d00eeaa5c8f90b4dab8028ce76ad89c
Jared Tate <13957390+JaredTate@users.noreply.github.com>
2024-05-15 16:30:00 -0600

    Test

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const config = {
     development: {
-      apiBaseUrl: 'http://localhost:5001',
-      wsBaseUrl: 'ws://localhost:5002',
+      apiBaseUrl: 'http://digibyte.io',
+      wsBaseUrl: 'ws://digibyte.io/ws',
     },
     production: {
       apiBaseUrl: 'http://digibyte.io',
@@ -9,6 +9,6 @@ const config = {
     },
   };
   
-  const env = process.env.NODE_ENV || 'production';
+  const env = process.env.NODE_ENV || 'development';
   
   export default config[env];
