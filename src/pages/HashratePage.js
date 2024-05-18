2169416b8dc22baf9ffc4b6e9df9572acf4ac519
Jared Tate <13957390+JaredTate@users.noreply.github.com>
2024-05-18 17:19:20 -0600

    Add More Info

--- src/pages/HashratePage.js
+++ src/pages/HashratePage.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { Container, Typography, Grid, Paper, Box } from '@mui/material';
+import { Container, Typography, Grid, Paper, Box, Divider } from '@mui/material';
 import styles from '../App.module.css';
 import config from '../config';
 
@@ -9,6 +9,14 @@ const HashratePage = () => {
   const [hashrates, setHashrates] = useState(
     algoNames.reduce((acc, algo) => ({ ...acc, [algo]: 0 }), {})
   );
+  const [averageBlockTimes, setAverageBlockTimes] = useState(
+    algoNames.reduce((acc, algo) => ({ ...acc, [algo]: 0 }), {})
+  );
+  const [totalBlocksByAlgo, setTotalBlocksByAlgo] = useState(
+    algoNames.reduce((acc, algo) => ({ ...acc, [algo]: 0 }), {})
+  );
+  const [networkAverageBlockTime, setNetworkAverageBlockTime] = useState(0);
+  const [totalBlocksInLastHour, setTotalBlocksInLastHour] = useState(0);
   const [totalHashrate, setTotalHashrate] = useState(0);
   const [isLoading, setIsLoading] = useState(true);
   const blocksRef = useRef([]);
@@ -27,13 +35,13 @@ const HashratePage = () => {
       if (message.type === 'recentBlocks') {
         console.log('Received recent blocks:', message.data);
         blocksRef.current = message.data;
-        calculateHashrates();
+        calculateHashratesAndBlockTimes();
         setIsLoading(false);
       } else if (message.type === 'newBlock') {
         console.log('Received new block:', message.data);
         blocksRef.current.unshift(message.data);
         blocksRef.current = blocksRef.current.slice(0, 240);
-        calculateHashrates();
+        calculateHashratesAndBlockTimes();
       }
     };
 
@@ -46,7 +54,7 @@ const HashratePage = () => {
     };
   }, []);
 
-  const calculateHashrates = () => {
+  const calculateHashratesAndBlockTimes = () => {
     const oneHourAgo = Date.now() - 60 * 60 * 1000;
     const hourBlocks = blocksRef.current.filter(
       (block) => block.timestamp * 1000 >= oneHourAgo
@@ -67,6 +75,29 @@ const HashratePage = () => {
 
     const total = Object.values(updatedHashrates).reduce((sum, rate) => sum + rate, 0);
     setTotalHashrate(total);
+
+    const totalBlocksInHour = hourBlocks.length;
+    setTotalBlocksInLastHour(totalBlocksInHour);
+
+    const networkAverageBlockTime = 60 * 60 / totalBlocksInHour;
+    setNetworkAverageBlockTime(networkAverageBlockTime);
+
+    const updatedAverageBlockTimes = algoNames.reduce((acc, algo) => {
+      const algoBlocks = hourBlocks.filter((block) => block.algo === algo);
+      const algoBlocksInHour = algoBlocks.length;
+      const algoAverageBlockTime = 60 * 60 / algoBlocksInHour;
+
+      return { ...acc, [algo]: algoAverageBlockTime };
+    }, {});
+
+    setAverageBlockTimes(updatedAverageBlockTimes);
+
+    const updatedTotalBlocksByAlgo = algoNames.reduce((acc, algo) => {
+      const algoBlocks = hourBlocks.filter((block) => block.algo === algo);
+      return { ...acc, [algo]: algoBlocks.length };
+    }, {});
+
+    setTotalBlocksByAlgo(updatedTotalBlocksByAlgo);
   };
 
   const formatHashrate = (hashrate) => {
@@ -79,13 +110,24 @@ const HashratePage = () => {
     return `${hashrate.toFixed(2)} ${units[index]}`;
   };
 
+  const formatBlockTime = (blockTime) => {
+    if (blockTime === 0) return 'N/A';
+    const minutes = Math.floor(blockTime / 60);
+    const seconds = blockTime % 60;
+    return `${minutes} min ${seconds.toFixed(0)} s`;
+  };
+
+  const formatTotalBlocks = (totalBlocks) => {
+    return totalBlocks.toLocaleString();
+  };
+
   return (
     <Container maxWidth="lg" className={styles.container}>
       <Typography variant="h4" component="h4" align="center" fontWeight="bold" gutterBottom sx={{ paddingTop: '10px' }}>
-        DigiByte Hashrate By Algo
+        DigiByte Hashrate By Algo Last Hour
       </Typography>
       <Typography variant="body1" component="p" align="center" gutterBottom>
-        This page displays the real-time hashrate for each DigiByte mining algorithm based on the blocks mined over the last hour.
+        This page displays the real-time hashrate, average block time & total blocks found for each DGB mining algorithm based on the blocks mined over the last hour.
         <br />
         Hashrate represents the total computational power being used to mine, process & secure transactions on the DigiByte blockchain.
       </Typography>
@@ -108,6 +150,26 @@ const HashratePage = () => {
                   <Typography variant="body1" className={styles.paragraph}>
                     The current hashrate for the {algo} algorithm.
                   </Typography>
+                  <Divider style={{ margin: '16px 0' }} />
+                  <Typography variant="h1" className={styles.centerText}>
+                    Total {algo} Blocks
+                  </Typography>
+                  <Typography variant="h2" className={`${styles.centerText} ${styles.boldText}`}>
+                    {formatTotalBlocks(totalBlocksByAlgo[algo])}
+                  </Typography>
+                  <Typography variant="body1" className={styles.paragraph}>
+                    The total {algo} blocks found in the last 60 minutes.
+                  </Typography>
+                  <Divider style={{ margin: '16px 0' }} />
+                  <Typography variant="h1" className={styles.centerText}>
+                    Avg. {algo} Block Time
+                  </Typography>
+                  <Typography variant="h2" className={`${styles.centerText} ${styles.boldText}`}>
+                    {formatBlockTime(averageBlockTimes[algo])}
+                  </Typography>
+                  <Typography variant="body1" className={styles.paragraph}>
+                    The average block time for {algo} over the last 60 minutes.
+                  </Typography>
                 </Paper>
               </Grid>
             ))}
@@ -121,6 +183,26 @@ const HashratePage = () => {
                 </Typography>
                 <Typography variant="body1" className={styles.paragraph} style={{ color: 'white' }}>
                   The combined hashrate of all 5 DGB mining algorithms.
+                </Typography>
+                <Divider style={{ margin: '16px 0', backgroundColor: 'white' }} />
+                <Typography variant="h1" className={styles.centerText} style={{ color: 'white' }}>
+                  Avg. Network Block Time
+                </Typography>
+                <Typography variant="h2" className={`${styles.centerText} ${styles.boldText}`} style={{ color: 'white' }}>
+                  {formatBlockTime(networkAverageBlockTime)}
+                </Typography>
+                <Typography variant="body1" className={styles.paragraph} style={{ color: 'white' }}>
+                  Average block time for the entire chain over the last 60 minutes.
+                </Typography>
+                <Divider style={{ margin: '16px 0', backgroundColor: 'white' }} />
+                <Typography variant="h1" className={styles.centerText} style={{ color: 'white' }}>
+                  Total Blocks in Last Hour
+                </Typography>
+                <Typography variant="h2" className={`${styles.centerText} ${styles.boldText}`} style={{ color: 'white' }}>
+                  {formatTotalBlocks(totalBlocksInLastHour)}
+                </Typography>
+                <Typography variant="body1" className={styles.paragraph} style={{ color: 'white' }}>
+                  The total number of blocks found in the last 60 minutes.
                 </Typography>
               </Paper>
             </Grid>
@@ -139,10 +221,10 @@ const HashratePage = () => {
               Here's what each part of the formula represents:
             </Typography>
             <Typography variant="body1" align="left" paragraph>
-              <strong>Blocks solved over last hour:</strong> The number of blocks mined by the specific algorithm in the last 60 minutes.
+              <strong>Blocks solved over last hour:</strong> The number of blocks solved by the algorithm in the last 60 minutes.
             </Typography>
             <Typography variant="body1" align="left" paragraph>
-              <strong>48:</strong> With a 15-second block time for each algorithm, the expected number of blocks per algorithm in a 1-hour period is 48.
+              <strong>48:</strong> There are 48 slots for blocks to be solved in one hour, given the target block time of 75 seconds per block.
             </Typography>
             <Typography variant="body1" align="left" paragraph>
               <strong>Difficulty * 2^32:</strong> The average difficulty of the blocks mined by the algorithm, multiplied by 2^32. This represents the average amount of work (hashes) needed to solve a block.
@@ -156,9 +238,9 @@ const HashratePage = () => {
           </Box>
         </>
       )}
-                  <Typography variant="h5" align="center" gutterBottom>
-              <strong>Hashrate units:</strong>
-            </Typography>
+      <Typography variant="h5" align="center" gutterBottom>
+        <strong>Hashrate units:</strong>
+      </Typography>
       <Typography variant="body1" align="center" paragraph>
         H/s - Hashes per second
         <br />
